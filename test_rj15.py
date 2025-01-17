5f84cbb9e156d756a0313c9abd506cbbd3acc3f3
Natalie Fernandes Spanghero <69602639+nataliespanghero@users.noreply.github.com>
2025-01-17 11:20:44 -0300

    Update test_rj15.py

--- test_rj15.py
+++ test_rj15.py
@@ -275,7 +275,7 @@ with tabs[0]:
         LayerControl().add_to(m)
 
     # Renderizar o mapa final
-    st_folium(m, width=800, height=600)
+    st_folium(m, width=None, height=600)
 
 # Aba 2: Gráfico
 with tabs[1]:
